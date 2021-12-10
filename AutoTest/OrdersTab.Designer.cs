331a83619bb2f036db42e885e29e5bb44704aef4
Анна Мусурина <anechkamusurina@gmail.com>
2021-12-10 12:53:09 +0300

    Добавьте файлы проекта.

---
+++ AutoTest/OrdersTab.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace AutoTest
+{
+    partial class OrdersTab
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrdersTab));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.DGVorders = new System.Windows.Forms.DataGridView();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.добавитьКлиентаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьСотрудникаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.автотестыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVorders)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(55, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Заказы";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(571, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 45;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(209, 48);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(301, 22);
+            this.txtSearch.TabIndex = 44;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.Location = new System.Drawing.Point(516, 40);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(40, 38);
+            this.btnSearch.TabIndex = 43;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // DGVorders
+            // 
+            this.DGVorders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVorders.Location = new System.Drawing.Point(22, 84);
+            this.DGVorders.Name = "DGVorders";
+            this.DGVorders.RowHeadersWidth = 51;
+            this.DGVorders.RowTemplate.Height = 24;
+            this.DGVorders.Size = new System.Drawing.Size(720, 344);
+            this.DGVorders.TabIndex = 42;
+            this.DGVorders.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVorders_CellContentClick);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Location = new System.Drawing.Point(775, 389);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(149, 39);
+            this.btnOrders.TabIndex = 41;
+            this.btnOrders.Text = "Добавить заказ";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьКлиентаToolStripMenuItem,
+            this.добавитьСотрудникаToolStripMenuItem,
+            this.автотестыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(936, 28);
+            this.menuStrip1.TabIndex = 46;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // добавитьКлиентаToolStripMenuItem
+            // 
+            this.добавитьКлиентаToolStripMenuItem.Name = "добавитьКлиентаToolStripMenuItem";
+            this.добавитьКлиентаToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.добавитьКлиентаToolStripMenuItem.Text = "Клиенты";
+            this.добавитьКлиентаToolStripMenuItem.Click += new System.EventHandler(this.добавитьКлиентаToolStripMenuItem_Click);
+            // 
+            // добавитьСотрудникаToolStripMenuItem
+            // 
+            this.добавитьСотрудникаToolStripMenuItem.Name = "добавитьСотрудникаToolStripMenuItem";
+            this.добавитьСотрудникаToolStripMenuItem.Size = new System.Drawing.Size(105, 24);
+            this.добавитьСотрудникаToolStripMenuItem.Text = "Сотрудники";
+            this.добавитьСотрудникаToolStripMenuItem.Click += new System.EventHandler(this.добавитьСотрудникаToolStripMenuItem_Click);
+            // 
+            // автотестыToolStripMenuItem
+            // 
+            this.автотестыToolStripMenuItem.Name = "автотестыToolStripMenuItem";
+            this.автотестыToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.автотестыToolStripMenuItem.Text = "Автотесты";
+            // 
+            // OrdersTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.DGVorders);
+            this.Controls.Add(this.btnOrders);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "OrdersTab";
+            this.Text = "Заказы";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVorders)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView DGVorders;
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьКлиентаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьСотрудникаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem автотестыToolStripMenuItem;
+    }
+}
+
---
+++ AutoTest/OrdersTab.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AutoTest
+{
+    public partial class OrdersTab : Form
+    {
+        public OrdersTab()
+        {
+            InitializeComponent();
+            this.Fill();
+        }
+        private void Fill()
+        {
+            DGVorders.DataSource = DBObjects.Entities.Orders.ToList();
+            this.DGVorders.Columns["Code_order"].Visible = false;
+            this.DGVorders.Columns["Code_client_ind"].Visible = false;
+            this.DGVorders.Columns["Code_client_org"].Visible = false;
+            this.DGVorders.Columns["Code_worker"].Visible = false;
+            this.DGVorders.Columns["Code_Autotest"].Visible = false;
+        }
+        private void добавитьКлиентаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            ClientTab formClient = new ClientTab();
+            formClient.ShowDialog();
+            this.Close();
+        }
+
+        private void добавитьСотрудникаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            WorkerTab formWorker = new WorkerTab();
+            formWorker.ShowDialog();
+            this.Close();
+        }
+
+        private void DGVorders_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Orders orders = (Orders)DGVorders.Rows[e.RowIndex].DataBoundItem;
+            //FormOrders formOrders = new FormOrders(orders);
+            //formOrders.ShowDialog();
+            Fill();
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            //FormOrders formOrders = new FormOrders(orders);
+            //formOrders.ShowDialog();
+            Fill();
+        }
+    }
+}
